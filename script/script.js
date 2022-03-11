b0bb9fa75687341470d16e011c90d74c1f0bed9f
Juris-Berkulis <krztfjrbhm1@jmail.com>
2022-03-11 15:07:34 +0300

    1. Add js-code, change html-code and add scss-code to change and display the minimum and maximum prices in the filter.

--- script/script.js
+++ script/script.js
@@ -3,3 +3,33 @@ const openImg = (build, imgNumber) => {
     img.src = `img/${build}_${imgNumber}.jpg`;
     img.alt = `${build}_${imgNumber}`;
 };
+
+const getValuesFromRangeInput = () => {
+    //* Get slider values:
+    const priceRangeInputList = document.getElementsByClassName("page__filterPriceRangeInput");
+    let priceRangeInputVal1 = parseFloat(priceRangeInputList[0].value);
+    let priceRangeInputVal2 = parseFloat(priceRangeInputList[1].value);
+
+    //* Neither slider will clip the other, so make sure we determine which is larger:
+    if (priceRangeInputVal1 > priceRangeInputVal2) {
+        const tmp = priceRangeInputVal2; 
+        priceRangeInputVal2 = priceRangeInputVal1; 
+        priceRangeInputVal1 = tmp;
+    }
+    
+    const minValueSpan = document.getElementsByClassName('page__filterPriceNumber-min')[0];
+    minValueSpan.innerHTML = `${priceRangeInputVal1}`;
+
+    const maxValueSpan = document.getElementsByClassName('page__filterPriceNumber-max')[0];
+    maxValueSpan.innerHTML = `${priceRangeInputVal2}`;
+};
+
+window.onload = () => {
+    //* Initialize Sliders:
+    const priceRangeInputList = document.getElementsByClassName("page__filterPriceRangeInput");
+    for(let i = 0; i < priceRangeInputList.length; i++) {
+        priceRangeInputList[i].oninput = getValuesFromRangeInput;
+        //* Manually trigger event first time to display values:
+        priceRangeInputList[i].oninput();
+    }
+};
